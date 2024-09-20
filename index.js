4eb4c67d3ab9615ccc3c107d08c74efc594a1f8a
E.V.SAI GANESH <141230623+saiganesh113@users.noreply.github.com>
2024-09-20 22:45:15 +0530

    changed port value

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ mongoose.connect(process.env.MONGO_URI
 )
 .then(() => {
   console.log('Connected to MongoDB successfully');
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+  app.listen(process.env.PORT || 6000, () => {
+    console.log(`Server running on port ${process.env.PORT || 6000}`);
   });
 })
 .catch((err) => {
